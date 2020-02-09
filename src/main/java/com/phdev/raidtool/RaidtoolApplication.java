babec887439cd5ba0c245dd6db6e50e920fc173c
Pratik Hingorani <hingorani.pratik@gmail.com>
2020-02-09 00:46:04 -0600

    init

---
+++ src/main/java/com/phdev/raidtool/RaidtoolApplication.java
@@ -0,0 +1,13 @@
+package com.phdev.raidtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phdev/raidtool/RaidtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phdev.raidtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
